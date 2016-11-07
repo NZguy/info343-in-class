44e234f8b268675f4d902e9afb8d8211a47aace5
emarkovic <enamarkovic27@gmail.com>
2016-11-07 14:30:23 -0800

    removed testing line in index.jsx

--- lab/react/src/index.jsx
+++ lab/react/src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {render} from "react-dom";
 
-import App from "./appSolution.jsx";
-// import App from "./app.jsx";
+import App from "./app.jsx";
 
 render(<App/>, document.getElementById("app"));
