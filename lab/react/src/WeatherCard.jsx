e25bb5a970c0489308420b228fde2694efe895d5
emarkovic <enamarkovic27@gmail.com>
2016-11-07 14:16:37 -0800

    react lab

---
+++ lab/react/src/WeatherCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {		
+		return <p className="center-text">Replace this with your weather display element.</p>;
+	}
+}
+		
