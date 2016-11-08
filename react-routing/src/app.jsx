3ee699d47a74001f26c12ccec703cdfdeda36644
Dave Stearns <dlsinfo@uw.edu>
2016-11-07 19:00:30 -0800

    adjusted the starter code for react routing day

--- react-routing/src/app.jsx
+++ react-routing/src/app.jsx
@@ -12,17 +12,6 @@ import React from "react";
 //directory 
 import "./css/main.css";
 
-//polyfill for the fetch() API so that we can use
-//it in Safari and older browsers
-//this module was already included in our package.json
-//so after you execute `npm install` this module will
-//be in the node_modules directory, so we can load it
-//simply by importing it's module name
-import "whatwg-fetch";
-
-const APIKEY = "...paste your api key here...";
-const DISCOVER_API = "https://api.themoviedb.org/3/discover/movie?api_key=" + APIKEY;
-
 export default class extends React.Component {
     constructor(props) {
         super(props);
@@ -32,9 +21,7 @@ export default class extends React.Component {
         return (
             <div>
                 <main>
-                    <div className="container">
-                        <h1>Hello React!</h1>
-                    </div>
+                    <h1>Hello React!</h1>
                 </main>
             </div>
         );
